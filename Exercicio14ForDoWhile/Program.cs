f72249cebf2ab6e352f8983b6e6cda54618b7c84
leandrodev1989 <leandrojose125327@gmail.com>
2022-05-17 18:15:21 -0300

    Exercicio14 finalizado

--- Exercicio14ForDoWhile/Program.cs
+++ Exercicio14ForDoWhile/Program.cs
@@ -1,1 +1,16 @@
-﻿/*14.Mostrar as potências de 2 variando de 0 a 10. /*
+﻿/*14.Mostrar as potências de 2 variando de 0 a 10. */
+
+/// <summary>
+/// Recebe a contagem da pontência elevado
+/// </summary>
+double PotenciaDe0A100;
+
+
+/// <summary>
+/// Processamento de dodos Com o i como contador
+/// </summary>
+for (int i = 0; i <= 10; i++)
+{
+    PotenciaDe0A100 = Math.Pow(2, i);
+    Console.WriteLine(PotenciaDe0A100);
+}
