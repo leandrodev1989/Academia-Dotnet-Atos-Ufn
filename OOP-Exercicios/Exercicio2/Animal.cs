bf5d161e8c9186e428e13cc02efb79df889452b6
leandrodev1989 <leandrojose125327@gmail.com>
2022-05-25 17:43:51 -0300

    crição do exercicio2

---
+++ OOP-Exercicios/Exercicio2/Animal.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio2
+{
+     class Animal
+     {
+        private int contadorgato = 0;
+        private int contadorcachorro = 0;
+        private int contadorpeixe = 0;
+
+        private string Nome { get;  set; }
+        private string Tipo { get;  set; }
+
+        public Animal(string nome, string tipo, int contadorgato, int contadorcachorro, int contadorpeixe)
+        {
+
+            this.Nome = nome;
+            this.Tipo = tipo;
+            this.contadorgato = contadorgato;
+            this.contadorcachorro = contadorcachorro;
+            this.contadorpeixe = contadorpeixe;
+            
+        }
+
+        public Animal()
+        {
+
+        }
+
+
+
+        public void ExibirGato()
+        {
+             Console.WriteLine($"NOME: {Nome}");
+             Console.WriteLine($"TIPO: {Tipo}");
+             Console.WriteLine($"NUMERO: {contadorgato}");
+            
+        }
+
+        public void Exibircachorro()
+        {
+            Console.WriteLine($"NOME: {Nome}");
+            Console.WriteLine($"TIPO: {Tipo}");
+            Console.WriteLine($"NUMERO: {contadorcachorro}");
+
+        }
+
+        public void ExibirPeixe()
+        {
+            Console.WriteLine($"NOME: {Nome}");
+            Console.WriteLine($"TIPO: {Tipo}");
+            Console.WriteLine($"NUMERO: {contadorpeixe}");
+
+        }
+
+
+
+    }
+
+    
+}
--- OOP-Exercicios/Exercicio2/Program.cs
+++ OOP-Exercicios/Exercicio2/Program.cs
@@ -1,2 +1,184 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*#2
+Crie um programa em C# no qual o usuário deverá informar o nome e o tipo de cinco animais de estimação. 
+O programa deverá exibir na tela para o usuário quantos Cachorros, Gatos e Peixes foram informados.
+
+Regras que deverão ser seguidas para a implementação do programa:
+
+Os únicos tipos de animais válidos são Cachorro, Gato, Peixe.
+Caso seja informado um tipo diferente o programa repetir até o usuário digitar um tipo válido.
+É obrigatório criar uma classe para representar o Animal.
+A classe deverá possuir dois dados privados (propriedades) para representar as características do animal.
+A classe deverá possuir métodos de acesso (propriedade getter e setter) para permitir que o usuário armazene/leia os dados dos dois dados privados (propriedades).*/
+
+
+namespace Exercicio2
+{
+    class Program
+    {
+        static List<Animal> animais = new List<Animal>();
+        static void Main(string[] args)
+        {
+            string opcao = "";
+
+            do
+            {
+                Console.Clear();
+
+               
+                Console.WriteLine("[ANIMAIS DE ESTIMAÇÃO]");
+                Console.WriteLine("1 - CADASTRAR ANIMAIS");
+                Console.WriteLine("2 - LISTAR Gato");
+                Console.WriteLine("3 - LISTAR Peixe");
+                Console.WriteLine("4 - LISTAR Cachorro");
+                Console.WriteLine("5 - SAIR");
+                Console.WriteLine("OPÇÃO");
+                opcao = Console.ReadLine();
+
+
+                switch (opcao)
+                {
+                    case "1":
+                        CadastrarTipoAnimais();
+                        break;
+                    case "2":
+                   
+                        ListarGato();
+                        break;
+                    case "3":
+                    case "4":
+
+                        ListarPeixe();
+                        break;
+                    case "5":
+
+                        ListarCachorro();
+                        break;
+                        Console.WriteLine("Obrigado por usar o sistema");
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida!");
+                        break;
+
+
+                }
+
+                Console.Write("Pressione algo para continuar!");
+                Console.ReadKey();
+
+            } while (opcao != "7");
+        }
+
+        static void CadastrarTipoAnimais()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                int contadorgato = 0;
+                int contadorcachorro = 0;
+                int contadorpeixe = 0;
+
+                Console.WriteLine("Entre com os Dados dos  Animais");
+                Console.WriteLine("Digite o nome do Animal");
+                var nome = Console.ReadLine();
+
+                Console.WriteLine("Digite o Tipo do Animal");
+                var tipo = Console.ReadLine();
+                Console.WriteLine("");
+
+
+                if (tipo == "GATO".ToLower() || tipo == "CACHORRO".ToLower() || tipo == "PEIXE".ToLower())
+                {
+                    if(tipo == "gato")
+                    {
+                        contadorgato++;
+                    }
+                    else if (tipo == "cachorro")
+                    {
+                        contadorcachorro++;
+                    }
+                    else if(tipo == "peixe")
+                    {
+                        contadorpeixe++;
+                    }
+
+
+                    Animal animal = new Animal(nome, tipo, contadorgato, contadorcachorro, contadorpeixe);
+
+                    
+                    animais.Add(animal);
+
+
+                  
+                }
+                else
+                {
+                    Console.WriteLine("Tipo de Animal Não Valido!!");
+
+                }
+
+               
+
+
+
+
+
+            }
+
+
+        }
+
+        static void ListarGato()
+        {
+
+            Console.WriteLine("Lista dos animais");
+
+            foreach( var i in animais)
+            {
+               i.ExibirGato();
+               
+
+            }
+             
+               
+           
+
+        }
+
+        static void ListarCachorro()
+        {
+
+            Console.WriteLine("Lista dos animais");
+
+            foreach (var i in animais)
+            {
+              
+                i.Exibircachorro();
+               
+            }
+
+
+
+
+        }
+
+        static void ListarPeixe()
+        {
+
+            Console.WriteLine("Lista dos animais");
+
+            foreach (var i in animais)
+            {
+               
+                i.ExibirPeixe();
+
+            }
+
+
+
+
+        }
+
+
+
+
+    }
+}
