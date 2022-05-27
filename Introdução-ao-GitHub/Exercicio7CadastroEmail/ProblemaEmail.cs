c2ff024fdafc6ffeb2a3725870f21e93506655ee
leandrodev1989 <leandrojose125327@gmail.com>
2022-05-27 16:54:07 -0300

    alteração

---
+++ Introdução-ao-GitHub/Exercicio7CadastroEmail/ProblemaEmail.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio6CadastroEmail
+{
+    class ProbleEmail
+    {
+        string nomeCompleto { get; set; }
+        string email { get; set; }
+
+       
+
+        public ProbleEmail(string nome)
+        {
+            this.nomeCompleto = nome;
+            this.gerarEmail();
+
+        }
+
+
+
+        private void gerarEmail()
+        {
+            string[] vetorDados = this.nomeCompleto.Split(' ');
+            this.email = vetorDados[vetorDados.Length - 1] + "," + vetorDados[0] + "@ufn.edu.br";
+            this.email = this.email.ToLower();
+        }
+
+    }
+}
