45d45ce3e15e03971b8a299d7946f8c05830bb0d
leandrodev1989 <leandrojose125327@gmail.com>
2022-05-30 09:12:45 -0300

    alteração feita

--- Estruturas_Condicionais_Controle/exercicio2/Program.cs
+++ Estruturas_Condicionais_Controle/exercicio2/Program.cs
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-﻿
 
 /*Faça um algoritmo para ler um número e verificar se ele é par ou ímpar.*/
 
@@ -24,30 +22,3 @@ else
 {
     Console.WriteLine("O numero é impar");
 }
-=======
-﻿
-
-/*Faça um algoritmo para ler um número e verificar se ele é par ou ímpar.*/
-
-
-/// <summary>
-/// Variável responsavel por receber a entrada do numero
-/// </summary>
-int numero;
-
-
-//Entrada de Dados
-Console.Write("Digite Um Numero: ");
-numero = int.Parse(Console.ReadLine());
-
-
-//Processamento de Dados
-if (numero % 2 == 0)
-{
-    Console.WriteLine("O numero é Par");
-}
-else
-{
-    Console.WriteLine("O numero é impar");
-}
->>>>>>> 36533bca4632f67392c486c79c73fa6a9a276d22
