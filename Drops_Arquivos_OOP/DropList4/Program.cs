a133e5439444496678d2d3729be95b1090ebb71f
leandrodev1989 <leandrojose125327@gmail.com>
2022-05-30 09:05:21 -0300

    alteração

--- Drops_Arquivos_OOP/DropList4/Program.cs
+++ Drops_Arquivos_OOP/DropList4/Program.cs
@@ -23,7 +23,6 @@ string opcao;
 do
 {
     Console.Clear();
-<<<<<<< HEAD
     Console.WriteLine("==============================  - MENU                                    =======================================");
     Console.WriteLine("==============================  - MENU                                    =======================================");
     Console.WriteLine("==============================1 - Abrir arquivo com emails e popular lista=======================================");
@@ -32,15 +31,6 @@ do
     Console.WriteLine("==============================4 - Cadastrar emails                        =======================================");
     Console.WriteLine("==============================5 - Sair                                    =======================================");
     Console.Write    ("==============================  - Opção:   ");
-=======
-    Console.WriteLine("MENU");
-    Console.WriteLine("1 - Abrir arquivo com emails e popular lista");
-    Console.WriteLine("2 - Exibir lista de emails");
-    Console.WriteLine("3 - Mostrar os domínios dos emails");
-    Console.WriteLine("4 - Cadastrar Email");
-    Console.WriteLine("5 - Sair");
-    Console.Write("Opção: ");
->>>>>>> a6e1ea9bbf1db576408ce8b13c7dbbf106352d58
     opcao = Console.ReadLine();
 
     switch (opcao)
