e77cc78524aab95758a0e28cf0d283081fbea869
leandrodev1989 <leandrojose125327@gmail.com>
2022-07-01 22:41:27 -0300

    primeira aula de Aspnet MVC

---
+++ Conteudo_Back_End_Professor_Ricardo/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public string Welcome(string nome, int vezes = 1, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"oi {nome}, vezes: {vezes}, ID: {ID}");
+        }
+
+        public string Teste()
+        {
+            return "aqui é só para mostrar";
+        }
+
+    }
+}
