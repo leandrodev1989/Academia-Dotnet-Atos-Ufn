2138c8c4dfc899384c89c4870f301fa9c5ba83f7
leandrodev1989 <leandrojose125327@gmail.com>
2022-07-04 13:08:05 -0300

    alteração

--- Conteudo_Front_End_Professor_Deiveson/Aula_javascript/script.js
+++ Conteudo_Front_End_Professor_Deiveson/Aula_javascript/script.js
@@ -1,3 +1,44 @@
+document.getElemteById('formulario-horizontal').addEventListener('submit', function (e){
+  e.preventDefault();
+  document.getElementById('tabela-dados'),classList.remove('d-none');
+  console.log('Submetido');
+});
+
+document.getElementById('nome').addEventListener('keyup', gerarLogin);
+document.getElementById('sobrenome').addEventListener('keyup', gerarLogin);
+
+
+function gerarLogin(){
+    const nome =  document.getElementById('nome').value;
+    const sobrenome = document.getElementById('sobrenome').value;
+    const login = nome + '.' + sobrenome;
+    document.getElementById('login').value = login.toLowerCase();
+}
+
+function limpa_formulario_cep() {
+    //Limpa valores do formulário de cep.
+    document.getElementById('endereco').value=("");
+    document.getElementById('bairro').value=("");
+    document.getElementById('cidade').value=("");
+    document.getElementById('estado').value=("");
+    
+}
+
+function meu_callback(conteudo) {
+if (!("erro" in conteudo)) {
+    //Atualiza os campos com os valores.
+    document.getElementById('endereco').value=(conteudo.logradouro);
+    document.getElementById('bairro').value=(conteudo.bairro);
+    document.getElementById('cidade').value=(conteudo.cidade);
+    document.getElementById('estado').value=(conteudo.uf);
+} //end if.
+else {
+    //CEP não Encontrado.
+    limpa_formulario_cep();
+    alert("CEP não encontrado.");
+    document.getElementById('cep').value=("");
+}
+}
 
 
 function pesquisacep(valor) {
@@ -41,3 +82,16 @@ function pesquisacep(valor) {
         limpa_formulario_cep();
     }
 }
+
+function formatar(mascara, documento){
+    var i = documento.value.length;
+    var saida = mascara.substring(0,1);
+    var texto = mascara.substring(i);
+    
+    if (texto.substring(0,1) != saida){
+              documento.value += texto.substring(0,1);
+    }
+    
+  }
+
+
